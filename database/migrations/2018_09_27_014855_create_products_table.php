36ca51757a5e1eea40f2f3dfb6e9a61bc38b3278
Kaio Goncalves Carvalho <kaio.carvalho@pravaler.com.br>
2018-09-26 23:01:41 -0300

    Primeira Versão do Laravel com Migrations e Docker

---
+++ database/migrations/2018_09_27_014855_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 100);
+            $table->string('description', 255);
+            $table->float('price');
+            $table->integer('quantity')->unsigned();
+            $table->timestampsTz();
+            $table->softDeletesTz();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
---
+++ database/migrations/2018_09_27_015127_create_stock_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStockTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stock', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('product_id')->unsigned();
+            $table->integer('quantity');
+            $table->integer('next_quantity');
+            $table->integer('previous_quantity');
+
+            $table->foreign('product_id')->references('id')->on('products');
+
+            $table->timestampsTz();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stock');
+    }
+}
